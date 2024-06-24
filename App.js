f5fa9beb1584985f1606e4c5d95b7fb4d1f468ad
Sarah Heinz. <124076207+SarahHeinz@users.noreply.github.com>
2024-06-24 23:33:39 +0200

    final commit

---
+++ App.js
@@ -0,0 +1,21 @@
+
+// ------------NAV MENU
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function closeNav() {
+  var links = document.getElementById("myLinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+}
+
